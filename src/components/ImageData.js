41e712e430015d58aa3547704471b22122100e46
Jeeranunnat <jeeranun.work@gmail.com>
2023-10-10 23:27:33 +0700

    git init

---
+++ src/components/ImageData.js
@@ -0,0 +1,23 @@
+const ImageData = [
+  {
+    title: 'แพนด้า',
+    image:
+      'https://cdn.pixabay.com/photo/2023/08/05/15/42/panda-8171354_1280.jpg',
+  },
+  {
+    title: 'สุนัข',
+    image:
+      'https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_1280.jpg',
+  },
+  {
+    title: 'กระรอก',
+    image:
+      'https://cdn.pixabay.com/photo/2023/08/24/17/34/squirrel-8211238_1280.jpg',
+  },
+  {
+    title: 'ผีเสื้อ',
+    image:
+      'https://cdn.pixabay.com/photo/2023/08/28/04/33/butterfly-8218330_1280.jpg',
+  },
+];
+export default ImageData;
